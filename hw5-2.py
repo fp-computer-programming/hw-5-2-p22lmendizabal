0e391bbe4b85f94570118a0a0874c9755ba32be6
p22mendizabal <p22lmendizabal@fairfieldprep.org>
2021-10-30 21:13:25 -0400

    add file

---
+++ hw5-2.py
@@ -0,0 +1,23 @@
+# author : LM (AMDG) 10/29/21
+import math
+# Question 1
+squareroot = math.sqrt(529)
+print(squareroot)
+# Question 2
+print(math.sin(0))
+# Question 3
+print(math.cos(2 * math.pi))
+# Question 4
+print(math.pow(2.5, 5))
+# Question 5
+print(math.log(math.e))
+# Question 6
+print(math.pow(math.pi, 4))
+# Question 7
+print(math.ceil(-12.9))
+# Question 8
+print(math.floor(7.25))
+# Question 9
+print(math.log(1000) * 2)
+# Question 10
+print(math.radians(180))
